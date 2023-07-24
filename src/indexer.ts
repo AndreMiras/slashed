4a8908bf4d4334e371b2a032c176e9447c196fc4
Andre Miras <andre.miras@gmail.com>
2023-07-24 10:31:40 +0200

    :recycle: refactor via processBlockResults

--- src/indexer.ts
+++ src/indexer.ts
@@ -1,9 +1,10 @@
 import assert from "assert";
 import * as dotenv from "dotenv";
-import _ from 'lodash';
+import _ from "lodash";
 import {
   Tendermint34Client,
   Event as BlockEvent,
+  BlockResultsResponse,
 } from "@cosmjs/tendermint-rpc";
 
 dotenv.config();
@@ -23,22 +24,26 @@ const logBlockEvent = (blockEvent: BlockEvent) => {
   }
 };
 
+const processBlockResults = (blockResults: BlockResultsResponse) => {
+  const beginBlockEvents = blockResults.beginBlockEvents;
+  const slashEvents = beginBlockEvents.filter(
+    (event) => event.type === "slash",
+  );
+  if (slashEvents.length !== 0) {
+    console.log("Slash event(s) at block", blockResults.height);
+    slashEvents.forEach((slashEvent) => logBlockEvent(slashEvent));
+  }
+};
+
 const main = async () => {
   const client = await Tendermint34Client.connect(TENDERMINT_RPC_URL);
   const startHeight = 5148552;
   const endHeight = startHeight + FETCH_BATCH_SIZE;
-  const promises = _.range(startHeight, endHeight + 1).map(height => client.blockResults(height));
+  const promises = _.range(startHeight, endHeight + 1).map((height) =>
+    client.blockResults(height),
+  );
   const blockResultsList = await Promise.all(promises);
-  blockResultsList.forEach((blockResults) => {
-    const beginBlockEvents = blockResults.beginBlockEvents;
-    const slashEvents = beginBlockEvents.filter(
-      (event) => event.type === "slash",
-    );
-    if (slashEvents.length !== 0) {
-      console.log("Slash event(s) at block", blockResults.height);
-      slashEvents.forEach((slashEvent) => logBlockEvent(slashEvent));
-    }
-  });
+  blockResultsList.forEach(processBlockResults);
   client.disconnect();
 };
 
