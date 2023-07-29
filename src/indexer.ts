912c463bcdb278e11dbc5f29571042c3f119e35d
Andre Miras <andre.miras@gmail.com>
2023-07-29 18:34:38 +0200

    :recycle: Split batch sizes

--- src/indexer.ts
+++ src/indexer.ts
@@ -24,37 +24,86 @@ const logBlockEvent = (blockEvent: BlockEvent) => {
   }
 };
 
-const processBlockResults = (blockResults: BlockResultsResponse) => {
-  const beginBlockEvents = blockResults.beginBlockEvents;
-  const slashEvents = beginBlockEvents.filter(
-    (event) => event.type === "slash",
-  );
-  if (slashEvents.length !== 0) {
-    console.log("Slash event(s) at block", blockResults.height);
-    slashEvents.forEach((slashEvent) => logBlockEvent(slashEvent));
-  }
-};
+const getSlashEventsForBlockResults = (
+  blockResults: BlockResultsResponse,
+): BlockEvent[] =>
+  blockResults.beginBlockEvents.filter((event) => event.type === "slash");
 
-const processBlocks = async (client: Tendermint34Client, heights: number[]) => {
+const getSlashEvents = async (
+  client: Tendermint34Client,
+  heights: number[],
+): Promise<Record<number, BlockEvent[]>> => {
   const promises = heights.map((height) => client.blockResults(height));
   const blockResultsList = await Promise.all(promises);
-  blockResultsList.forEach(processBlockResults);
+  const slashEvents = blockResultsList.reduce((slashEvents, blockResults) => {
+    const slashEventsForBlockResults =
+      getSlashEventsForBlockResults(blockResults);
+    const newSlashEvents =
+      slashEventsForBlockResults.length > 0
+        ? { [blockResults.height]: slashEventsForBlockResults }
+        : {};
+    return { ...slashEvents, ...newSlashEvents };
+  }, {});
+  return slashEvents;
 };
 
-const processBlockRange = async (
+const processBlocks = (
+  client: Tendermint34Client,
+  heights: number[],
+): Promise<Record<number, BlockEvent[]>> => getSlashEvents(client, heights);
+
+/**
+ * Processes blocks from startHeight to endHeight (inclusive).
+ */
+export const processBlockRange = async (
   client: Tendermint34Client,
   startHeight: number,
   endHeight: number,
-) => {
+): Promise<Record<number, BlockEvent[]>> => {
   const heights = _.range(startHeight, endHeight + 1);
-  processBlocks(client, heights);
+  return processBlocks(client, heights);
+};
+
+/**
+ * Processes blocks from startHeight to endHeight (inclusive) by batchSize chunks.
+ */
+const processBlockRangeChunks = async (
+  client: Tendermint34Client,
+  startHeight: number,
+  endHeight: number,
+  batchSize: number,
+): Promise<Record<number, BlockEvent[]>> => {
+  const allHeights = _.range(startHeight, endHeight + 1);
+  const heightsChunks = _.chunk(allHeights, batchSize);
+  const slashEventsWithEmpty = await Promise.all(
+    heightsChunks.map((heights) => processBlocks(client, heights)),
+  );
+  const slashEvents = slashEventsWithEmpty.reduce(
+    (acc, curr) => ({ ...acc, ...curr }),
+    {},
+  );
+  return slashEvents;
+};
+
+const logSlashEvents = (slashEvents: Record<number, BlockEvent[]>) => {
+  const slashHeights = _.sortBy(Object.keys(slashEvents).map(Number));
+  slashHeights.forEach((slashHeight: number) => {
+    console.log("Slash event(s) at block", slashHeight);
+    slashEvents[slashHeight].forEach((slashEvent) => logBlockEvent(slashEvent));
+  });
 };
 
 const main = async () => {
   const client = await Tendermint34Client.connect(TENDERMINT_RPC_URL);
   const startHeight = 5148552;
-  const endHeight = startHeight + FETCH_BATCH_SIZE;
-  processBlockRange(client, startHeight, endHeight);
+  const endHeight = startHeight + 100;
+  const slashEvents = await processBlockRangeChunks(
+    client,
+    startHeight,
+    endHeight,
+    FETCH_BATCH_SIZE,
+  );
+  logSlashEvents(slashEvents);
   client.disconnect();
 };
 
